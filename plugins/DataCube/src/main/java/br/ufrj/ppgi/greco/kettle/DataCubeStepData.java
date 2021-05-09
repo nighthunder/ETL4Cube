9ca9c930ce1a8ab3bcadce187d2b0198fedf8402
mayara-morais <mayara.morais@macroplan.com.br>
2021-05-09 04:42:29 -0300

    DataCube que funciona no Kettle 8.1

--- plugins/DataCube/src/main/java/br/ufrj/ppgi/greco/kettle/DataCubeStepData.java
+++ plugins/DataCube/src/main/java/br/ufrj/ppgi/greco/kettle/DataCubeStepData.java
@@ -4,9 +4,7 @@ import org.pentaho.di.core.row.RowMetaInterface;
 import org.pentaho.di.trans.step.BaseStepData;
 import org.pentaho.di.trans.step.StepDataInterface;
 
-public class DataCubeStepData extends BaseStepData implements
-        StepDataInterface
-{
+public class DataCubeStepData extends BaseStepData implements StepDataInterface {
 
-    public RowMetaInterface outputRowMeta;
+	public RowMetaInterface outputRowMeta;
 }
